98fa4904582fc812bb3fed6efe29d1ca50ad4cb4
Rafael Langoni <langoninaves@gmail.com>
2024-11-11 08:34:01 -0330

    creating Dockerfile

--- src/main/java/com/langonifood/order/OrderApplication.java
+++ src/main/java/com/langonifood/order/OrderApplication.java
@@ -2,10 +2,9 @@ package com.langonifood.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class OrderApplication {
 
 	public static void main(String[] args) {
